348ee42df7fd6f6a10ce43e43ed6701b6f239787
Nigel <nigel.coelho1@gmail.com>
2014-02-22 16:24:52 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Imagenet::Application.config.secret_key_base = '6a3c0f3dfad085214a4c4251514e0db92eb9881f44be00122201da4da2f801850dddac9e240fd431c8a3c20886c957af5fb3720040cd9737f6d78911f7bf1a1d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Imagenet::Application.config.session_store :cookie_store, key: '_imagenet_session'
